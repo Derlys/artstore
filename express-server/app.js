3a3f94169acaae7d4435056d9a448f25cb4f9132
Derlys Dominguez <derlys275@gmail.com>
2020-09-30 12:45:34 -0500

    express server node

---
+++ express-server/app.js
@@ -0,0 +1,31 @@
+let express = require("express"),
+  cors = require("cors");
+let app = express();
+app.use(express.json());
+app.use(cors());
+app.listen(3000, () => console.log("server running on port 3000"));
+
+let ciudades = [
+  "Paris",
+  "Barcelona",
+  "Barranquilla",
+  "Monte video",
+  "Santiago de chile",
+  "Mexico DF",
+  "Nueva York",
+];
+app.get("/ciudades", (req, res, next) =>
+  res.json(
+    ciudades.filter(
+      (c) => c.toLowerCase().indexOf(req.query.q.toString().toLowerCase()) > -1
+    )
+  )
+);
+
+let misProducts = [];
+app.get("/my", (req, res, next) => res.json(misProducts));
+app.post("/my", (req, res, next) => {
+  console.log(req.body);
+  misProducts = req.body;
+  res.json(misProducts);
+});
